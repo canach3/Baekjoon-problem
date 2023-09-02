4190f4f4f9b51fc40df1994cf473ecc4596c06fb
김정윤 <127319680+canach3@users.noreply.github.com>
2023-09-02 22:22:38 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int score;
+	char grade;
+
+	cin >> score;
+
+	switch (score / 10)
+	{
+	case 10:
+	case 9:
+		grade = 'A';
+		break;
+	case 8:
+		grade = 'B';
+		break;
+	case 7:
+		grade = 'C';
+		break;
+	case 6: 
+		grade = 'D';
+		break;
+	default:
+		grade = 'F';
+		break;
+	}
+	cout << grade << endl;
+	
+	return 0;
+}
