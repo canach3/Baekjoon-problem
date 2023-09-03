7d24863c0e6280489cddef312cd31973b4fc7950
김정윤 <127319680+canach3@users.noreply.github.com>
2023-09-03 20:44:19 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	int sum;
+
+	cin >> n;
+	sum = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+		sum += i;
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
