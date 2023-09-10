2e71158920540304c6bf8b06bc99cc2c0fe8aaf6
김정윤 <127319680+canach3@users.noreply.github.com>
2023-09-10 21:38:22 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int arr[9];
+	int max = 0;
+	int idx;
+
+	for (int i = 0; i < 9; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] > max)
+		{
+			max = arr[i];
+			idx = i + 1;
+		}
+	}
+
+	cout << max << endl;
+	cout << idx << endl;
+
+	return 0;
+}
