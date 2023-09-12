486743c38f71bd92b3588aea95c856dd3788f143
김정윤 <127319680+canach3@users.noreply.github.com>
2023-09-12 12:23:18 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String alpha = sc.next();
+        char ch = alpha.charAt(0);
+
+        System.out.println((int)ch);
+    }
+}
