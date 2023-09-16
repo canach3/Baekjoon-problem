2cbbeccbe66a7942f179be3da0ea726d7105ad7e
김정윤 <127319680+canach3@users.noreply.github.com>
2023-09-17 01:01:50 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
- 
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
- 
-		int[] arr = new int[26];
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = -1;
-		}
- 
-		String S = in.nextLine();
- 
-		for(int i = 0; i < S.length(); i++) {
-			char ch = S.charAt(i);
-    
-			if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 인 경우에만 초기화
-				arr[ch - 'a'] = i;
-			}
-		}
- 
-		for(int val : arr) {	// 배열 출력
-			System.out.print(val + " ");
-		}
-	}
+    public static void main(String[] args) {
+
+        String [] alpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        int [] alphaIndex = new int[26];
+        Arrays.fill(alphaIndex, -1);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < alphaIndex.length; j++) {
+                if (alphaIndex[j] != -1) {
+                    continue;
+                }
+                if (input.substring(i,i+1).equals(alpha[j])) {
+                    alphaIndex[j] = i;
+                }
+            }
+        }
+        for (int i = 0; i < alphaIndex.length; i++) {
+            System.out.print(alphaIndex[i] + " ");
+        }
+        sc.close();
+    }
 }
