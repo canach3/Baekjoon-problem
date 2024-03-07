edd3eb01cd21d657df2240cf4799f7286c722acb
김정윤 <127319680+canach3@users.noreply.github.com>
2024-03-07 20:25:35 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.cc
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -3,14 +3,11 @@ using namespace std;
 
 int main() {
 
-	int n;
-	cin >> n;
+	int N;
+	cin >> N;
 
-	for (int i = 1; i < 10; i++)
+	for (int i = 1; i <= 9; i++)
 	{
-		cout << n << " * " << i << " = " << n * i << endl;
+		cout << N << " * " << i << " = " << N * i << endl;
 	}
-
-
-	return 0;
 }
