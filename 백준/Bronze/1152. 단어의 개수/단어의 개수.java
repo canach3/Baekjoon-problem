701a74b286ee08ef09c5f2e805f76b87b33e2b4c
김정윤 <canach3@gmail.com>
2024-03-18 16:16:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 488 ms, Memory: 30580 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int count = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                count++;
+            }
+        }
+
+        if (s.charAt(0) == ' ') {
+            count--;
+        }
+
+        if (s.charAt(s.length() - 1) == ' ') {
+            count--;
+        }
+
+        System.out.println(count);
+    }
+}
