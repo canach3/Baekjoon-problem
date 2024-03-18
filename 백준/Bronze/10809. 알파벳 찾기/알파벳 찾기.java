a9437373b9061b44609000b6fbb5b0e2d27b05e1
김정윤 <canach3@gmail.com>
2024-03-18 16:02:51 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 224 ms, Memory: 18264 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,25 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String [] alpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-        int [] alphaIndex = new int[26];
-        Arrays.fill(alphaIndex, -1);
-
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
 
-        for (int i = 0; i < input.length(); i++) {
-            for (int j = 0; j < alphaIndex.length; j++) {
-                if (alphaIndex[j] != -1) {
-                    continue;
-                }
-                if (input.substring(i,i+1).equals(alpha[j])) {
-                    alphaIndex[j] = i;
-                }
-            }
+        int alphabet[] = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = -1;
         }
-        for (int i = 0; i < alphaIndex.length; i++) {
-            System.out.print(alphaIndex[i] + " ");
+
+        String s = sc.next();
+
+        for (int i = 0; i < s.length(); i++) {
+           if (alphabet[(int) s.charAt(i) - 97] == -1) {
+               alphabet[(int) s.charAt(i) - 97] = i;
+           }
         }
-        sc.close();
+
+        for (int i = 0; i < 26; i++) {
+            System.out.print(alphabet[i] + " ");
+        }
     }
 }
