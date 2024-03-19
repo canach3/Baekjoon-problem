53dc23a832ba08293b5b53e2d64aef22263d0861
김정윤 <canach3@gmail.com>
2024-03-19 16:52:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -2,13 +2,10 @@
 using namespace std;
 
 int main() {
-	
 	cout << "         ,r'\"7" << endl;
 	cout << "r`-_   ,'  ,/" << endl;
 	cout << " \\. \". L_r'" << endl;
 	cout << "   `~\\/" << endl;
 	cout << "      |" << endl;
 	cout << "      |" << endl;
-
-	
 }
