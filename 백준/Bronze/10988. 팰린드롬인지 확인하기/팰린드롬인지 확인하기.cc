ad22ea716335cc546190eb78960cfb88cfbaa1fa
김정윤 <canach3@gmail.com>
2024-03-19 17:42:18 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,29 +1,18 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-	
-	string word;
-	cin >> word;
+	string s;
+	cin >> s;
 
-	bool ispalindorme = true;
-
-	for (int i = 0; i < word.length() / 2; i++)
+	for (int i = 0; i < s.length() / 2; i++)
 	{
-		if (word[i] != word[word.length() - 1 - i])
+		if (s[i] != s[s.length() - 1 - i])
 		{
-			ispalindorme = false;
-			break;
+			cout << 0 << endl;
+			return 0;
 		}
 	}
 
-	if (ispalindorme)
-	{
-		cout << 1 << endl;
-	}
-	else {
-		cout << 0 << endl;
-	}
-	
+	cout << 1 << endl;
 }
