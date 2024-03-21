6ab8f6251e84aa75edd93a90e91cac0d3f9a5858
김정윤 <canach3@gmail.com>
2024-03-21 14:02:52 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.cc
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.cc
@@ -1,65 +1,33 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main() {
-	
-	string subject;
-	string grade;
+	string subjectName;
 	double credit;
-	double creditSum = 0;
-	double sum = 0;
-	
-	
-	
+	string grade;
+	double totalScore = 0;
+	double totalCredit = 0;
+	int count = 20;
+
 	for (int i = 0; i < 20; i++)
 	{
-		cin >> subject >> credit >> grade;
+		cin >> subjectName >> credit >> grade;
 
-		if (grade == "P")
+		totalCredit += credit;
+
+		if (grade == "A+") totalScore += credit * 4.5;
+		else if (grade == "A0") totalScore += credit * 4.0;
+		else if (grade == "B+") totalScore += credit * 3.5;
+		else if (grade == "B0") totalScore += credit * 3.0;
+		else if (grade == "C+") totalScore += credit * 2.5;
+		else if (grade == "C0") totalScore += credit * 2.0;
+		else if (grade == "D+") totalScore += credit * 1.5;
+		else if (grade == "D0") totalScore += credit * 1.0;
+		else if (grade == "F") totalScore += credit * 0.0;
+		else if (grade == "P")
 		{
-			continue;
-		}
-
-		creditSum += credit;
-
-		if (grade == "A+")
-		{
-			sum += credit * 4.5;
-		}
-		else if (grade == "A0") {
-			sum += credit * 4.0;
-			
-		}
-		else if (grade == "B+") {
-			sum += credit * 3.5;
-			
-		}
-		else if (grade == "B0") {
-			sum += credit * 3.0;
-			
-		}
-		else if (grade == "C+") {
-			sum += credit * 2.5;
-			
-		}
-		else if (grade == "C0") {
-			sum += credit * 2.0;
-			
-		}
-		else if (grade == "D+") {
-			sum += credit * 1.5;
-			
-		}
-		else if (grade == "D0") {
-			sum += credit * 1.0;
-		
-		}
-		else if (grade == "F") {
-			sum += credit * 0.0;
-			
+			totalCredit -= credit;
 		}
 	}
-	cout << fixed << setprecision(6);
-	cout << sum / creditSum << endl;
+	cout << totalScore / totalCredit;
 }
