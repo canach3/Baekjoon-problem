38c9548b2dde15dd9a3ec6fe782e8d4fae1493be
김정윤 <canach3@gmail.com>
2024-03-23 08:44:04 +0900

    [Bronze I] Title: 세로읽기, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Main {
@@ -6,20 +5,20 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        char[][]word = new char[5][15];
+        char[][] arr = new char[5][15];
         String input;
 
         for (int i = 0; i < 5; i++) {
             input = sc.nextLine();
             for (int j = 0; j < input.length(); j++) {
-                word[i][j] = input.charAt(j);
+                arr[i][j] = input.charAt(j);
             }
         }
 
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                if (word[j][i] != '\u0000') {
-                    System.out.print(word[j][i]);
+                if (arr[j][i] != '\0') {
+                    System.out.print(arr[j][i]);
                 }
             }
         }
