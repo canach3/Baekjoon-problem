a2c19403857ebcfaf7936fbd1dc73b46f0cb14e5
김정윤 <canach3@gmail.com>
2024-05-02 14:36:13 +0900

    [Silver V] Title: 색종이, Time: 224 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] paper = new int[100][100];
+        int sum = 0;
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (paper[j][k] == 0) {
+                        paper[j][k] = 1;
+                        sum++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
