6b99b120c3d9fdecaf624a57dd38dce05eaba82b
김정윤 <canach3@gmail.com>
2024-05-19 08:03:43 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 608 ms, Memory: 23864 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 2 * n; j > i * 2; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = n - 1; j >= i; j--) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i * 2; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = n - 1; j >= i; j--) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
