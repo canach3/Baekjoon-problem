045c04e38db355a1c3b6fc1bfa3ba0a15840811f
김정윤 <canach3@gmail.com>
2024-05-26 07:36:59 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 1)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				cout << "* ";
+			}
+
+			cout << endl;
+		}
+
+		if (i % 2 == 0)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				cout << " *";
+			}
+
+			cout << endl;
+		}
+	}
+}
