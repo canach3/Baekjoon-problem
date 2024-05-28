794e56d6b8c814a32e0dca5bbb0624b285bf9b49
김정윤 <canach3@gmail.com>
2024-05-29 07:56:13 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n1, n2, n3, n4, n5;
+	cin >> n1 >> n2 >> n3 >> n4 >> n5;
+
+	int verification = (n1 * n1 + n2 * n2 + n3 * n3 + n4 * n4 + n5 * n5) % 10;
+
+	cout << verification << endl;
+}
