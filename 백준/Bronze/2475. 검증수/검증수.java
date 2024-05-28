b3b52a96b43f7a39d924f51422fd1c3e0e9eae61
김정윤 <canach3@gmail.com>
2024-05-29 07:58:07 +0900

    [Bronze V] Title: 검증수, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();
+        int n5 = sc.nextInt();
+
+        int verification = (n1*n1 + n2*n2 + n3*n3 + n4*n4 + n5*n5) % 10;
+
+        System.out.println(verification);
+    }
+}
