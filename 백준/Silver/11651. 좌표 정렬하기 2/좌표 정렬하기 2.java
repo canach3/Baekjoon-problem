c315baff54198ab6041b6b3f61ca1968f6208245
김정윤 <canach3@gmail.com>
2024-07-06 21:43:08 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1828 ms, Memory: 184844 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, comparator);
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+
+    static Comparator<int[]> comparator = new Comparator<int[]>() {
+        @Override
+        public int compare(int[] arr1, int[] arr2) {
+            if (arr1[1] == arr2[1]) {
+                return arr1[0] - arr2[0];
+            }
+            return arr1[1] - arr2[1];
+        }
+    };
+}
