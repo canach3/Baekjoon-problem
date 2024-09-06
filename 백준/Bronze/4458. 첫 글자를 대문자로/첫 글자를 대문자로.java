08239d6bb062ac58666f9e8118e3ee85f84adaff
김정윤 <canach3@gmail.com>
2024-09-06 13:04:18 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 164 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++)
+        {
+            String temp = sc.nextLine();
+            String result = temp.substring(0, 1).toUpperCase() + "" + temp.substring(1, temp.length());
+            System.out.println(result);
+        }
+    }
+}
