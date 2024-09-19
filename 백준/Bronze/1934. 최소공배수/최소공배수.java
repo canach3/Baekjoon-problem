c0b9bfa70f3a0975d8665e1436721534bee405a6
김정윤 <canach3@gmail.com>
2024-09-19 09:54:52 +0900

    [Bronze I] Title: 최소공배수, Time: 284 ms, Memory: 22984 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(lcm(a, b));
+        }
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
