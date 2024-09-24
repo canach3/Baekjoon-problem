eef8c69b79e100f8574975fe226cf90a22794abb
김정윤 <canach3@gmail.com>
2024-09-25 06:45:32 +0900

    [Silver II] Title: 베르트랑 공준, Time: 924 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
 
         while (true) {
             int n = sc.nextInt();
