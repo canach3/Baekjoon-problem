8b0bf8c3169188d9496a2a305ec237428c415882
김정윤 <canach3@gmail.com>
2024-10-11 07:36:08 +0900

    [Bronze I] Title: 이항 계수 1, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int result = factorial(N) / (factorial(N-K) * factorial(K));
+        System.out.println(result);
+    }
+
+    static int factorial(int n) {
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
