7927a631d5808461aa287b2570b57fe19d53907e
김정윤 <canach3@gmail.com>
2025-01-26 10:02:38 +0900

    [Bronze II] Title: 수 정렬하기, Time: 288 ms, Memory: 22272 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,18 +1,25 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int N = sc.nextInt();
+        int[] numArr = new int[N];
 
-        int[] numArr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             numArr[i] = sc.nextInt();
         }
 
-        Arrays.sort(numArr);
+        for (int i = N; i > 1; i--) {
+            for (int j = 1; j < i; j++) {
+                if (numArr[j] <= numArr[j - 1]) {
+                    int temp = numArr[j];
+                    numArr[j] = numArr[j - 1];
+                    numArr[j - 1] = temp;
+                }
+            }
+        }
 
         for (int num : numArr) {
             System.out.println(num);
