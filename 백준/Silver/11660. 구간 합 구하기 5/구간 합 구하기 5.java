e85b819ec0440753b53a3834b9a61c180cacf947
김정윤 <canach3@gmail.com>
2025-07-15 11:52:13 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2672 ms, Memory: 328592 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,25 +1,18 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // N, M 입력 받기
         int N = sc.nextInt();
         int M = sc.nextInt();
-
-        // N 크기의 2차원 배열 생성, 값 집어넣기
         int[][] numArr = new int[N+1][N+1];
-        for (int i = 1; i < N+1; i++) {
-            for (int j = 1; j < N+1; j++) {
-                numArr[i][j] = sc.nextInt();
-            }
-        }
-
-        // 구간합 2차원 배열 생성, 값 집어넣기
         int[][] sumArr = new int[N+1][N+1];
-        for (int i = 1; i < N+1; i++) {
-            for (int j = 1; j < N+1; j++) {
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                numArr[i][j] = sc.nextInt();
                 sumArr[i][j] = sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] + numArr[i][j];
             }
         }
@@ -30,8 +23,7 @@ public class Main {
             int x2 = sc.nextInt();
             int y2 = sc.nextInt();
 
-            int result = sumArr[x2][y2] - sumArr[x2][y1-1] - sumArr[x1-1][y2] + sumArr[x1-1][y1-1];
-         
+            int result = sumArr[x2][y2] - sumArr[x1-1][y2] - sumArr[x2][y1-1] + sumArr[x1-1][y1-1];
             System.out.println(result);
         }
     }
