75a410576866d08ca82f1109287e2c9bb7e63b46
김정윤 <canach3@gmail.com>
2025-07-18 09:52:32 +0900

    [Gold IV] Title: 좋다, Time: 328 ms, Memory: 22164 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,39 +1,46 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        // O(N^2) 까지는 괜찮음
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-
         int[] numArr = new int[N];
+
         for (int i = 0; i < N; i++) {
             numArr[i] = sc.nextInt();
         }
         Arrays.sort(numArr);
 
         int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            int checkNum = numArr[i];
-            int startIdx = 0;
-            int endIdx = N-1;
 
-            while (startIdx < endIdx) {
-                int sum = numArr[endIdx] + numArr[startIdx];
-                if (sum < checkNum) {
-                    startIdx++;
-                } else if (sum > checkNum) {
-                    endIdx--;
-                } else {
-                    if (startIdx != i && endIdx != i) {
-                        cnt++;
-                        break;
-                    } else if (startIdx == i) {
-                        startIdx++;
-                    } else {
-                        endIdx--;
-                    }
+        for (int i = 0; i < N; i++) {
+            int start = 0;
+            int end = N-1;
+
+            while (start < end) {
+                if (start == i) {
+                    start++;
+                    continue;
+                }
+
+                if (end == i) {
+                    end--;
+                    continue;
+                }
+
+                if (numArr[start] + numArr[end] == numArr[i]) {
+                    cnt++;
+                    break;
+                }
+
+                if (numArr[start] + numArr[end] < numArr[i]) {
+                    start++;
+                }
+
+                if (numArr[start] + numArr[end] > numArr[i]) {
+                    end--;
                 }
             }
         }
