a28f630d5f6bfe417074338221d4f4a0c74d23f0
김정윤 <canach3@gmail.com>
2025-07-21 09:56:30 +0900

    [Silver IV] Title: 카드2, Time: 232 ms, Memory: 32068 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,20 +1,21 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Deque<Integer> deque = new ArrayDeque<>();
+        int N = sc.nextInt();
 
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            deque.offerLast(i);
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
         }
 
-        while (deque.size() != 1) {
-            deque.pop();
-            deque.offerLast(deque.pop());
+        while (deque.size() > 1) {
+            deque.pollFirst();
+            deque.addLast(deque.pollFirst());
         }
 
         System.out.println(deque.peek());
