ff2b5d0df3dca8c54fd49fc39fb7522cebb3e81b
김정윤 <canach3@gmail.com>
2025-07-25 08:43:37 +0900

    [Silver V] Title: K번째 수, Time: 3336 ms, Memory: 461596 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,26 +1,26 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		List<Integer> A = new ArrayList<>(); 
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		Collections.sort(A);
-		
-		System.out.println(A.get(K-1));
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-	}
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
 
+        st = new StringTokenizer(in.readLine());
+        for(int i = 0; i < N; i++) {
+            minheap.add(Integer.parseInt(st.nextToken()));
+        }
+
+        for(int i = 0; i < K; i++) {
+            if(i == K-1) {
+                System.out.println(minheap.peek());
+            }
+            minheap.poll();
+        }
+    }
 }
