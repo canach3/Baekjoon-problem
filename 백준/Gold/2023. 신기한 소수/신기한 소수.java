3c905cf3fe44d8cd5ae677090ab4b27007445efa
김정윤 <canach3@gmail.com>
2025-07-31 09:37:43 +0900

    [Gold V] Title: 신기한 소수, Time: 172 ms, Memory: 17516 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,41 +1,50 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N;
+    static StringBuilder sb;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
+
         N = sc.nextInt();
 
-        DFS(2, 1);
-        DFS(3, 1);
-        DFS(5, 1);
-        DFS(7, 1);
+        for (int i = 2; i <= 9; i++) {
+            int digit = 1;
+
+            if (i == 2 || i == 3 || i == 5 || i == 7) {
+                DFS(i, digit);
+            }
+        }
+
+        System.out.println(sb);
     }
 
-    static void DFS(int num, int digit) {
-        if (digit == N) {
-            if (isPrime(num)) {
-                System.out.println(num);
-            }
-
+    static void DFS(int i, int digit) {
+        if (digit >= N) {
+            sb.append(i).append("\n");
             return;
         }
 
-        for (int i = 1; i <= 9; i += 2) {
-            if (isPrime(num * 10 + i)) {
-                DFS(num * 10 + i, digit + 1);
+        digit++;
+
+        for (int j = 1; j <= 9; j++) {
+            int num = i * 10 + j;
+
+            if(isPrime(num)) {
+                DFS(num, digit);
             }
         }
     }
 
-    static boolean isPrime(int n) {
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+    static boolean isPrime(int i) {
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
                 return false;
             }
         }
-
         return true;
     }
 }
