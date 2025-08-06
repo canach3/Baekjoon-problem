6560733f3cb8c5ca9aad75c33aad50927e5490e9
김정윤 <canach3@gmail.com>
2025-08-06 10:51:27 +0900

    [Silver I] Title: 기타 레슨, Time: 284 ms, Memory: 24228 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,50 +1,52 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int start = 0;
-        int end = 0;
-        int[] array = new int[N];
+        int[] lectureArr = new int[N];
+        int max = 0;
+        int total = 0;
 
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int input = sc.nextInt();
-            array[i] = input;
-            end += input;
-
-            if (input >= start) {
-                start = input;
-            }
+            lectureArr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, lectureArr[i]);
+            total += lectureArr[i];
         }
 
-        while (start <= end) {
-            int middle = (end + start) / 2;
-            int sum = 0;
-            int cnt = 0;
+        int left = max;
+        int right = total;
+        int result = total;
 
-            for (int i = 0; i < array.length; i++) {
-                if (sum + array[i] > middle) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int sum = 0;
+            int cnt = 1;
+
+            for (int i = 0; i < N; i++) {
+                if (sum + lectureArr[i] > mid) {
                     cnt++;
                     sum = 0;
                 }
-                sum += array[i];
-            }
-
-            if (sum != 0) {
-                cnt++;
+                sum += lectureArr[i];
             }
 
             if (cnt > M) {
-                start = middle + 1;
-            } else {
-                end = middle - 1;
+                left = mid + 1;
+            }
+
+            if (cnt <= M) {
+                result = mid;
+                right = mid - 1;
             }
         }
 
-        System.out.println(start);
+        System.out.println(result);
     }
 }
