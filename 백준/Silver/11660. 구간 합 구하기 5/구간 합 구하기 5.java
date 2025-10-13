50d332cfa60631facb28364a47f78c061843dc4d
김정윤 <canach3@gmail.com>
2025-10-13 09:10:02 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1376 ms, Memory: 129644 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,28 +2,38 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[][] numArr = new int[N+1][N+1];
-        int[][] sumArr = new int[N+1][N+1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] numArr = new int[N + 1][N + 1];
+        int[][] sumArr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                numArr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                numArr[i][j] = sc.nextInt();
-                sumArr[i][j] = sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] + numArr[i][j];
+                sumArr[i][j] = numArr[i][j] + sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1];
             }
         }
 
         for (int i = 0; i < M; i++) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
 
-            int result = sumArr[x2][y2] - sumArr[x1-1][y2] - sumArr[x2][y1-1] + sumArr[x1-1][y1-1];
+            int result = sumArr[y2][x2] - sumArr[y2][x1-1] - sumArr[y1-1][x2] + sumArr[y1-1][x1-1];
+
             System.out.println(result);
         }
     }
