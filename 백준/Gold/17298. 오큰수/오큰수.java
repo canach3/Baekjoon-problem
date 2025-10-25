9a9079b6935c93e3e262a311d5c57bb4e3b56dba
김정윤 <canach3@gmail.com>
2025-10-25 10:05:52 +0900

    [Gold IV] Title: 오큰수, Time: 792 ms, Memory: 156040 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,43 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
+        Node[] nodeArr = new Node[N];
 
-        Node[] nodeArr = new Node[N+1];
-        for (int i = 1; i <= N; i++) {
-            nodeArr[i] = new Node(i, sc.nextInt());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            nodeArr[i] = new Node(Integer.parseInt(st.nextToken()));
         }
 
-        Deque<Node> deque = new ArrayDeque<>();
+        Deque<Node> nodeDeque = new ArrayDeque<>();
 
-        for (int i = 1; i <= N; i++) {
-            while (!deque.isEmpty() && deque.peekLast().value < nodeArr[i].value) {
-                deque.pollLast().NGE = nodeArr[i].value;
+        for (int i = 0; i < N - 1; i++) {
+            nodeDeque.addFirst(nodeArr[i]);
+
+            while (!nodeDeque.isEmpty() && nodeDeque.peekFirst().value < nodeArr[i + 1].value) {
+                nodeDeque.removeFirst().NGE = nodeArr[i + 1].value;
             }
-            deque.addLast(nodeArr[i]);
         }
 
-        while (!deque.isEmpty()) {
-            deque.pollLast().NGE = -1;
+        while (!nodeDeque.isEmpty()) {
+            nodeDeque.removeFirst().NGE = -1;
         }
 
-        for (int i = 1; i <= N; i++) {
+        nodeArr[N-1].NGE = -1;
+
+        for (int i = 0; i < N; i++) {
             sb.append(nodeArr[i].NGE).append(" ");
         }
 
         System.out.println(sb);
     }
-    static class Node{
-        int index;
+
+    static class Node {
         int value;
         int NGE;
 
-        public Node(int index, int value) {
-            this.index = index;
+        Node(int value) {
             this.value = value;
         }
     }
