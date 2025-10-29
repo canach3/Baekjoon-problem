26cd91ba94538855ed3151bc6ca0729b9fdddec1
김정윤 <canach3@gmail.com>
2025-10-29 09:23:43 +0900

    [Gold V] Title: ABCDE, Time: 228 ms, Memory: 20072 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -2,65 +2,58 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static List<Integer>[] adjList;
     static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> people = new ArrayList<>();
     static boolean result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N];
-
+        adjList = new List[N];
         for (int i = 0; i < N; i++) {
-            people.add(new ArrayList<>());
+            adjList[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int p1 = Integer.parseInt(st.nextToken());
-            int p2 = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            people.get(p1).add(p2);
-            people.get(p2).add(p1);
+            adjList[a].add(b);
+            adjList[b].add(a);
         }
 
-        int depth = 0;
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                visited[j] = false;
-            }
-
-            depth = 1;
-
-            DFS(i, depth);
+            visited = new boolean[N];
+            DFS(i, 1);
 
             if (result) {
                 System.out.println(1);
                 return;
             }
         }
+
         System.out.println(0);
     }
 
-    static void DFS(int person, int depth) {
-        visited[person] = true;
-
-        if (depth >= 5) {
+    static void DFS(int current, int cnt) {
+        if (cnt >= 5) {
             result = true;
             return;
         }
 
-        for (int i = 0; i < people.get(person).size(); i++) {
-            int friend = people.get(person).get(i);
+        visited[current] = true;
 
-            if (!visited[friend]) {
-                DFS(friend, depth + 1);
-                visited[friend] = false;
+        for (int i = 0; i < adjList[current].size(); i++) {
+            int next = adjList[current].get(i);
+
+            if (!visited[next]) {
+                DFS(next, cnt + 1);
+                visited[next] = false;
             }
         }
     }
