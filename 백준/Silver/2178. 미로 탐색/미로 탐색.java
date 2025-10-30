a93bf6d23b08bcbd8a8dbb3f33d2a428301baf04
김정윤 <canach3@gmail.com>
2025-10-30 10:10:27 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Node[][] nodes;
     static boolean[][] visited;
+    static int[][] maze;
     static int N;
     static int M;
 
@@ -14,83 +14,64 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        nodes = new Node[N + 2][M + 2];
         visited = new boolean[N + 2][M + 2];
+        maze = new int[N + 2][M + 2];
 
-        for (int i = 0; i <= N + 1; i++) {
-            for (int j = 0; j <= M + 1; j++) {
-                nodes[i][j] = new Node(0, j, i);
-            }
-        }
-
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             String input = br.readLine();
-            char[] chars = input.toCharArray();
-
-            for (int j = 1; j <= chars.length; j++) {
-                nodes[i][j].value = chars[j - 1] - '0';
+            for (int j = 0; j < M; j++) {
+                maze[i + 1][j + 1] = input.charAt(j) - '0';
             }
         }
 
-        BFS(nodes[1][1]);
+        BFS(1, 1);
     }
 
-    static void BFS(Node startNode) {
-        visited[startNode.y][startNode.x] = true;
+    static void BFS(int y, int x) {
+        int cnt = 1;
+        visited[y][x] = true;
         Deque<Node> deque = new ArrayDeque<>();
-        deque.addLast(startNode);
-
-        startNode.cnt = 1;
+        deque.addFirst(new Node(y, x, 1));
 
         while (!deque.isEmpty()) {
-            Node currentNode = deque.removeFirst();
+            Node current = deque.removeLast();
 
-            if (currentNode.x == M && currentNode.y == N) {
-                System.out.println(currentNode.cnt);
+            if (current.y == N && current.x == M) {
+                System.out.println(current.cnt);
                 return;
             }
 
-            Node upNode = nodes[currentNode.y-1][currentNode.x]; // 상
-            Node downNode = nodes[currentNode.y+1][currentNode.x]; // 하
-            Node leftNode = nodes[currentNode.y][currentNode.x-1]; // 좌
-            Node rightNode = nodes[currentNode.y][currentNode.x+1]; // 우
-
-            if (upNode.value == 1 && !visited[upNode.y][upNode.x]) {
-                visited[upNode.y][upNode.x] = true;
-                upNode.cnt = currentNode.cnt + 1;
-                deque.addLast(upNode);
+            if (maze[current.y - 1][current.x] == 1 && !visited[current.y - 1][current.x]) {
+                deque.addFirst(new Node(current.y - 1, current.x, current.cnt + 1));
+                visited[current.y - 1][current.x] = true;
             }
 
-            if (downNode.value == 1 && !visited[downNode.y][downNode.x]) {
-                visited[downNode.y][downNode.x] = true;
-                downNode.cnt = currentNode.cnt + 1;
-                deque.addLast(downNode);
+            if (maze[current.y + 1][current.x] == 1 && !visited[current.y + 1][current.x]) {
+                deque.addFirst(new Node(current.y + 1, current.x, current.cnt + 1));
+                visited[current.y + 1][current.x] = true;
             }
 
-            if (leftNode.value == 1 && !visited[leftNode.y][leftNode.x]) {
-                visited[leftNode.y][leftNode.x] = true;
-                leftNode.cnt = currentNode.cnt + 1;
-                deque.addLast(leftNode);
+            if (maze[current.y][current.x - 1] == 1 && !visited[current.y][current.x - 1]) {
+                deque.addFirst(new Node(current.y, current.x - 1, current.cnt + 1));
+                visited[current.y][current.x - 1] = true;
             }
 
-            if (rightNode.value == 1 && !visited[rightNode.y][rightNode.x]) {
-                visited[rightNode.y][rightNode.x] = true;
-                rightNode.cnt = currentNode.cnt + 1;
-                deque.addLast(rightNode);
+            if (maze[current.y][current.x + 1] == 1 && !visited[current.y][current.x + 1]) {
+                deque.addFirst(new Node(current.y, current.x + 1, current.cnt + 1));
+                visited[current.y][current.x + 1] = true;
             }
         }
-    }
 
+    }
     static class Node {
-        int value;
-        int x;
         int y;
+        int x;
         int cnt;
 
-        public Node(int value, int x, int y) {
-            this.value = value;
-            this.x = x;
+        Node(int y, int x, int cnt) {
             this.y = y;
+            this.x = x;
+            this.cnt = cnt;
         }
     }
 }
