26c95f4a0251ad4e2e07ed2ce59259f417dfcd44
김정윤 <canach3@gmail.com>
2026-01-01 14:45:13 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,22 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int[][] paper = new int[100][100];
+        int[][] map = new int[101][101];
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+        st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    map[x+j][y+k] = 1;
+                }
+            }
+        }
+
         int sum = 0;
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-
-            for (int j = x; j < x + 10; j++) {
-                for (int k = y; k < y + 10; k++) {
-                    if (paper[j][k] == 0) {
-                        paper[j][k] = 1;
-                        sum++;
-                    }
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                if (map[i][j] == 1) {
+                    sum++;
                 }
             }
         }
