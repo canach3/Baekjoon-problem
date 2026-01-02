f16defbde96d68b62a29ec324b728102b9b7621c
김정윤 <canach3@gmail.com>
2026-01-03 00:13:51 +0900

    [Silver IV] Title: 수열, Time: 220 ms, Memory: 23080 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int current;
+		int max = 1;
+		
+		// up
+		current = 1;
+		for (int i = 1; i < N; i++) {
+			if (arr[i] < arr[i - 1]) {
+				current = 1;
+			} else {
+				current++;
+			}
+			
+			if (current > max) {
+				max = current;
+			}
+		}
+		
+		
+		// down
+		current = 1;
+		for (int i = 1; i < N; i++) {
+			if (arr[i] > arr[i - 1]) {
+				current = 1;
+			} else {
+				current++;
+			}
+			
+			if (current > max) {
+				max = current;
+			}
+		}
+		
+		
+		System.out.println(max);
+	}
+}
