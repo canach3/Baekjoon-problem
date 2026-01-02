86747d81e1dcefefb0be92778740799780ce37e3
김정윤 <canach3@gmail.com>
2026-01-02 13:02:47 +0900

    [Silver III] Title: 자리배정, Time: 172 ms, Memory: 19752 KB -BaekjoonHub

---
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int C = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		int[][] map = new int[R][C];
+		
+		int x = 0;
+		int y = R - 1;
+		int order = 1;
+		int current = 4;
+		
+		while (x >= 0 && x <= C - 1 && y >= 0 && y <= R-1 && map[y][x] == 0) {
+			map[y][x] = order++;
+			
+			if (current == 4) {
+				if (y <= 0 || map[y - 1][x] != 0) {
+					current = 1;
+					x++;
+				} else {
+					y--;
+				}
+			} else if (current == 1) {
+				if (x >= C-1 || map[y][x + 1] != 0) {
+					current = 3;
+					y++;
+				} else {
+					x++;
+				}
+			} else if (current == 3) {
+				if (y >= R-1 || map[y + 1][x] != 0) {
+					current = 2;
+					x--;
+				} else {
+					y++;
+				}
+			} else if (current == 2) {
+				if (x <= 0 || map[y][x - 1] != 0) {
+					current = 4;
+					y--;
+				} else {
+					x--;
+				}
+			}
+		}
+		
+		int K = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if(map[i][j] == K) {
+					System.out.println((j + 1) + " " + (R - i));
+					return;
+				}
+			}
+		}
+		
+		System.out.println(0);
+	}
+}
