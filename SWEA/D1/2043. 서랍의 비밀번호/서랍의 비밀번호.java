c108a7f9c6c032ee2e5880fc90df823609edb2ea
김정윤 <canach3@gmail.com>
2026-01-15 17:02:23 +0900

    [D1] Title: 서랍의 비밀번호, Time: 75 ms, Memory: 25,344 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int P = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(P - K + 1);
+    }
+}
