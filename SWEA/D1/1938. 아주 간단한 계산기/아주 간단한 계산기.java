45343dd246f50ab0f464fed281650a90f1035f12
김정윤 <canach3@gmail.com>
2026-01-15 17:25:26 +0900

    [D1] Title: 아주 간단한 계산기, Time: 78 ms, Memory: 25,216 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int result1 = a + b;
+        int result2 = a - b;
+        int result3 = a * b;
+        int result4 = a / b;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
