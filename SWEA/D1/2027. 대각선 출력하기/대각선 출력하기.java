77e673d5f14cf4e9aacfe8f2801d7249bd04aaa1
김정윤 <canach3@gmail.com>
2026-01-15 17:11:15 +0900

    [D1] Title: 대각선 출력하기, Time: 77 ms, Memory: 25,216 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        System.out.print(
+                "#++++\n" +
+                "+#+++\n" +
+                "++#++\n" +
+                "+++#+\n" +
+                "++++#");
+    }
+}
