c1ad3d243d40ebda5b7229e37c7fe3239c8df583
김정윤 <canach3@gmail.com>
2026-01-15 09:20:37 +0900

    [D1] Title: 최대수 구하기, Time: 77 ms, Memory: 25,856 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int max = 0;
+            for (int i = 0; i < 10; i++) {
+                int input = Integer.parseInt(st.nextToken());
+                if (input > max) {
+                    max = input;
+                }
+            }
+
+            System.out.println("#" + testCase + " " + max);
+        }
+    }
+}
