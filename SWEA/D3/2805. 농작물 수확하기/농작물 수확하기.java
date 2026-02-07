8837369dc6dfff46d427b606930795e1132097df
김정윤 <canach3@gmail.com>
2026-02-08 01:35:47 +0900

    [D3] Title: 농작물 수확하기, Time: 91 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -2,14 +2,22 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    static int N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int sum;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException{
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine().trim());
         for (int testCase = 1; testCase <= T; testCase++) {
-            int N = Integer.parseInt(br.readLine().trim());
-            int[][] map = new int[N][N];
+            N = Integer.parseInt(br.readLine().trim());
+            map = new int[N][N];
+            visited = new boolean[N][N];
 
             for (int i = 0; i < N; i++) {
                 String input = br.readLine().trim();
@@ -18,27 +26,27 @@ class Solution {
                 }
             }
 
-            int sum = 0;
-
-            for (int i = 0; i <= N / 2; i++) {
-                int j = N / 2 - i;
-                for (int time = 0; time < (i+1) * 2 - 1; time++) {
-                    sum += map[i][j];
-                    j++;
-                }
-            }
-
-            for (int i = 0; i < N / 2; i++) {
-                int j = i + 1;
-                for (int time = 0; time < N - 2 - (2 * i); time++) {
-                    sum += map[N/2 + 1 + i][j];
-                    j++;
-                }
-            }
+            sum = 0;
+            DFS(N / 2, N / 2);
 
             sb.append("#").append(testCase).append(" ").append(sum).append("\n");
         }
 
         System.out.print(sb);
+    }
+
+    static void DFS(int y, int x) {
+        visited[y][x] = true;
+        sum += map[y][x];
+
+        if (Math.abs(N/2 - y) + Math.abs(N/2 - x) >= N / 2) {
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (!visited[y + dy[i]][x + dx[i]]) {
+                DFS(y + dy[i], x + dx[i]);
+            }
+        }
     }
 }
