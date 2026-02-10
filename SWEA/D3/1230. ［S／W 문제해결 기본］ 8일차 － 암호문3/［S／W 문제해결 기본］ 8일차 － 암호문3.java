6cd029e075e7cee5f676165bd9af14dd310a489c
김정윤 <canach3@gmail.com>
2026-02-10 13:00:48 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 154 ms, Memory: 37,200 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int testCase = 1; testCase <= 10; testCase++) {
+            sb.append("#").append(testCase).append(" ");
+
+            int N = Integer.parseInt(br.readLine().trim());
+            String input = br.readLine().trim();
+            List<String> list = new LinkedList<>(Arrays.asList(input.split(" ")));
+
+            int M = Integer.parseInt(br.readLine().trim());
+            StringTokenizer st = new StringTokenizer(br.readLine().trim());
+            for (int i = 0; i < M; i++) {
+                char command = st.nextToken().charAt(0);
+                if (command == 'A') {
+                    int cnt = Integer.parseInt(st.nextToken());
+
+                    for (int j = 0; j < cnt; j++) {
+                        String num = st.nextToken();
+                        list.add(num);
+                    }
+                } else if (command == 'I') {
+                    int idx = Integer.parseInt(st.nextToken());
+                    int cnt = Integer.parseInt(st.nextToken());
+
+                    for (int j = 0; j < cnt; j++) {
+                        String num = st.nextToken();
+                        list.add(idx + j, num);
+                    }
+                } else if (command == 'D') {
+                    int idx = Integer.parseInt(st.nextToken());
+                    int cnt = Integer.parseInt(st.nextToken());
+
+                    for (int j = 0; j < cnt; j++) {
+                        list.remove(idx + j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                sb.append(list.get(i)).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
