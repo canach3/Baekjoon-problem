27f9e605378c3c1b8fd0cec62945db90bf417860
김정윤 <canach3@gmail.com>
2026-02-14 15:48:51 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 209 ms, Memory: 29,176 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Solution {
 	static int N;
 	static int[][] map;
-	static int[] selectA;
+	static boolean[] selectA; 
 	static int min;
 	
 	public static void main(String[] args) throws IOException {
@@ -13,8 +13,6 @@ public class Solution {
 		
 		int T = Integer.parseInt(br.readLine().trim());
 		for (int testCase = 1; testCase <= T; testCase++) {
-			sb.append("#" + testCase + " ");
-			
 			N = Integer.parseInt(br.readLine().trim());
 			map = new int[N][N];
 			
@@ -25,12 +23,12 @@ public class Solution {
 				}
 			}
 			
-			min = 1000000;
+			min = Integer.MAX_VALUE;
+			selectA = new boolean[N];
 			
-			selectA = new int[N];
 			DFS(0, 0);
 			
-			sb.append(min).append("\n");
+			sb.append("#").append(testCase).append(" ").append(min).append("\n");
 		}
 		
 		System.out.print(sb);
@@ -43,26 +41,27 @@ public class Solution {
 			
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if (selectA[i] == 1 && selectA[j] == 1) {
+					if (i == j) continue;
+					
+					if (selectA[i] && selectA[j]) {
 						sumA += map[i][j];
 					}
-					
-					if (selectA[i] == 0 && selectA[j] == 0) {
+					else if (!selectA[i] && !selectA[j]) {
 						sumB += map[i][j];
 					}
 				}
 			}
 			
-			if (Math.abs(sumA - sumB) < min) min = Math.abs(sumA - sumB);
+			min = Math.min(min, Math.abs(sumA - sumB));
 			return;
 		}
 		
 		if (ingredient >= N) return;
 		
-		selectA[ingredient] = 1;
+		selectA[ingredient] = true;
 		DFS(ingredient + 1, cnt + 1);
 		
-		selectA[ingredient] = 0;
+		selectA[ingredient] = false;
 		DFS(ingredient + 1, cnt);
 	}
 }
