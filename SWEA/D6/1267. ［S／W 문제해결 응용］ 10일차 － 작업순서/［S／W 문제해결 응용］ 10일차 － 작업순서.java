6f81b728bb8ec6fe35a155a2239180db8ba1f25a
김정윤 <canach3@gmail.com>
2026-02-18 14:46:32 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 161 ms, Memory: 33,152 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -2,64 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static List<List<Integer>> adjList;
-	static int time;
-	static int[][] nodeTime; // 각 노드를 방문한 시간을 저장하는 배열(discover time, finish time)
-	
-	public static void main(String[] args) throws IOException{
-		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for (int testCase = 1; testCase <= 10; testCase++) {
-			sb.append("#").append(testCase).append(" ");
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int V = Integer.parseInt(st.nextToken());
-			int E = Integer.parseInt(st.nextToken());
-			
-			adjList = new ArrayList<>();
-			nodeTime = new int[V + 1][3];
-			
-			for (int i = 0; i <= V; i++) {
-				adjList.add(new ArrayList<>());
-				nodeTime[i][0] = i;
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < E; i++) {
-				int start = Integer.parseInt(st.nextToken());
-				int end = Integer.parseInt(st.nextToken());
-				
-				adjList.get(start).add(end);
-			}
-			
-			time = 1;
-			for (int i = 1; i <= V; i++) {
-				if(nodeTime[i][1] == 0) DFS(i);
-			}
-			
-			Arrays.sort(nodeTime, (o1, o2) -> {
-				return Integer.compare(o2[2], o1[2]);
-			});
-			
-			for (int i = 0; i < V; i++) {
-				sb.append(nodeTime[i][0]).append(" ");
-			}
-			
-			sb.append("\n");
-		}
-				
-		System.out.print(sb);
-	}
-	
-	static void DFS(int current) {
-		nodeTime[current][1] = time++;
-		
-		for (int i = 0; i < adjList.get(current).size(); i++) {
-			int next = adjList.get(current).get(i);
-			if (nodeTime[next][1] == 0) DFS(next);
-		}
-		
-		nodeTime[current][2] = time++;
-	}
+    static List<List<Integer>> adjList;
+    static boolean[] visited;
+    static Stack<Integer> stack;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int testCase = 1; testCase <= 10; testCase++) {
+            sb.append("#").append(testCase).append(" ");
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            adjList = new ArrayList<>();
+            for (int i = 0; i <= V; i++) {
+                adjList.add(new ArrayList<>());
+            }
+            
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < E; i++) {
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                adjList.get(start).add(end);
+            }
+
+            visited = new boolean[V + 1];
+            stack = new Stack<>();
+
+            // 모든 노드에 대해 방문하지 않았다면 DFS 수행
+            for (int i = 1; i <= V; i++) {
+                if (!visited[i]) DFS(i);
+            }
+
+            // 스택에서 꺼내면 위상 정렬 순서 (종료 시간의 역순)
+            while (!stack.isEmpty()) {
+                sb.append(stack.pop()).append(" ");
+            }
+            
+            sb.append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+
+    static void DFS(int current) {
+        visited[current] = true;
+
+        for (int next : adjList.get(current)) {
+            if (!visited[next]) {
+                DFS(next);
+            }
+        }
+
+        // 더 이상 갈 곳이 없을 때(작업 완료), 스택에 넣음
+        // 가장 나중 작업이 스택 바닥에, 가장 먼저 해야 할 작업이 스택 꼭대기에 쌓임
+        stack.push(current);
+    }
 }
