697139f4778701f29c4e372bff7549dc41c88a34
김정윤 <canach3@gmail.com>
2026-02-18 21:44:42 +0900

    [D3] Title: 통나무 자르기, Time: 75 ms, Memory: 24,448 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			int N = Integer.parseInt(br.readLine().trim());
+			
+			String result = N % 2 == 0 ? "Alice" : "Bob";
+			
+			sb.append(result).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+}
