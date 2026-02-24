b4365a84cda89b469a883a76d55714e7cf98ee73
김정윤 <canach3@gmail.com>
2026-02-24 16:29:56 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 104 ms, Memory: 28,672 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.java
@@ -1,62 +1,67 @@
 import java.io.*;
 import java.util.*;
 
-class Solution {
-    static StringBuilder sb = new StringBuilder();
-    static Node[] tree;
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int testCase = 1; testCase <= 10; testCase++) {
-            sb.append("#").append(testCase).append(" ");
-
-            int N = Integer.parseInt(br.readLine().trim());
-            tree = new Node[N + 1];
-            for (int i = 1; i <= N; i++) {
-                tree[i] = new Node();
-            }
-
-            for (int i = 1; i <= N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int v = Integer.parseInt(st.nextToken());
-                String op = st.nextToken();
-
-                switch(op.charAt(0)) {
-                    case '+':
-                    case '-':
-                    case '*':
-                    case '/':
-                        tree[v].operator = op.charAt(0);
-                        tree[v].left = Integer.parseInt(st.nextToken());
-                        tree[v].right = Integer.parseInt(st.nextToken());
-                        break;
-                    default:
-                        tree[v].operand = Integer.parseInt(op);
-                        break;
-                }
-            }
-
-            sb.append((int) inOrder(1));
-            sb.append("\n");
-        }
-
-        System.out.print(sb);
-    }
-
-    static class Node {
-        int operand;
-        char operator;
-        int left;
-        int right;
-    }
-
-    static double inOrder(int v) {
-        if (tree[v].left == 0 && tree[v].right == 0) return tree[v].operand;
-
-        if (tree[v].operator == '+') return inOrder(tree[v].left) + inOrder(tree[v].right);
-        else if (tree[v].operator == '-') return inOrder(tree[v].left) - inOrder(tree[v].right);
-        else if (tree[v].operator == '*') return inOrder(tree[v].left) * inOrder(tree[v].right);
-        else return inOrder(tree[v].left) / inOrder(tree[v].right);
-    }
-}
+public class Solution {
+	static int N;
+	static List<int[]> adjList;
+	
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			N = Integer.parseInt(br.readLine().trim());
+			
+			adjList = new ArrayList<>();
+			
+			for (int i = 0; i <= N; i++) {
+				adjList.add(new int[3]);
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				
+				int V = Integer.parseInt(st.nextToken());
+				String input = st.nextToken();
+				char op = input.charAt(0);
+				
+				if (op == '+' || op == '-' || op == '*' || op == '/') {
+					adjList.get(V)[0] = op;
+					adjList.get(V)[1] = Integer.parseInt(st.nextToken());
+					adjList.get(V)[2] = Integer.parseInt(st.nextToken());
+				} else {
+					adjList.get(V)[0] = Integer.parseInt(input);
+				}
+			}
+			
+			double result = DFS(1);
+			sb.append((int) result).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+	
+	static double DFS(int num) {
+		int[] node = adjList.get(num);
+		
+		if(node[1] == 0) {
+			return node[0];
+		} else {
+			char operator = (char) node[0];
+			
+			switch(operator) {
+				default:
+				case '+':
+					return DFS(node[1]) + DFS(node[2]);
+				case '-':
+					return DFS(node[1]) - DFS(node[2]);
+				case '*':
+					return DFS(node[1]) * DFS(node[2]);
+				case '/':
+					return DFS(node[1]) / DFS(node[2]);
+			}
+		}
+	}
+}	
