d0fcd530afd9147a5b7de5e777a60864eef017fe
김정윤 <canach3@gmail.com>
2026-03-04 13:41:52 +0900

    [D2] Title: 쉬운 거스름돈, Time: 78 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -2,24 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-        for (int testCase = 1; testCase <= T; testCase++) {
-            StringBuilder sb = new StringBuilder();
-
-            int type = 50000;
-            int N = Integer.parseInt(br.readLine());
-
-            for (int i = 0; i < 8; i++) {
-                sb.append(N / type).append(" ");
-                N %= type;
-                type /= (i % 2 == 0 ? 5 : 2);
-            }
-
-            System.out.println("#" + testCase);
-            System.out.println(sb);
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append("\n");
+			
+			int N = Integer.parseInt(br.readLine().trim());
+			
+			int[] coins = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+			int[] arr = new int[8];
+			
+			for (int i = 0; i < 8; i++) {
+				sb.append(N / coins[i]).append(" ");
+				N %= coins[i];
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.print(sb);
+	}
 }
