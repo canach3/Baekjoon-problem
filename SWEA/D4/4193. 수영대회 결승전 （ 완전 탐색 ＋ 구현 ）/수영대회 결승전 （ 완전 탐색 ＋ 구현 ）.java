cc2be03d292213d8591763f1a534956e816df9a8
김정윤 <canach3@gmail.com>
2026-03-04 17:23:05 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 78 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -4,11 +4,7 @@ import java.util.*;
 public class Solution {
 	static int N;
 	static int[][] map;
-	static int[] start;
-	static int[] end;
 	static boolean[][] visited;
-	static int min;
-	static int hCnt;
 	
 	static int[] dx = {0, 0, -1, 1};
 	static int[] dy = {-1, 1, 0, 0};
@@ -34,30 +30,21 @@ public class Solution {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 1; j <= N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					
-					if (map[i][j] == 2) {
-						hCnt++;
-					}
 				}
 			}
 			
-			start = new int[2];
+			int[] start = new int[2];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			start[0] = Integer.parseInt(st.nextToken()) + 1;
 			start[1] = Integer.parseInt(st.nextToken()) + 1;
 			
-			end = new int[2];
+			int[] end = new int[2];
 			st = new StringTokenizer(br.readLine());
 			end[0] = Integer.parseInt(st.nextToken()) + 1;
 			end[1] = Integer.parseInt(st.nextToken()) + 1;	
 			
-			min = Integer.MAX_VALUE;
 			visited = new boolean[N + 2][N + 2];
-			visited[start[0]][start[1]] = true;
-			
-			DFS(start[0], start[1], 0);
-			
-			if (min == Integer.MAX_VALUE) min = -1;
+			int min = BFS(start[0], start[1], end[0], end[1]);
 			
 			sb.append(min).append("\n");
 		}
@@ -65,30 +52,45 @@ public class Solution {
 		System.out.print(sb);
 	}
 	
-	static void DFS(int y, int x, int time) {
-		// 이미 최소보다 경로가 긴 경우
-		if (time > min || time > N * N + (hCnt * 2)) return;
+	static int BFS(int startY, int startX, int endY, int endX) {
+		visited[startY][startX] = true;
 		
-		// 목적지에 도달한 경우
-		if (y == end[0] && x == end[1]) {
-			min = time;
-			return;
-		}
-	
-		// 경계를 벗어난 경우, 장애물인 경우
-		if (map[y][x] == -1 || map[y][x] == 1) return;
+		Deque<int[]> deque = new ArrayDeque<>();
+		deque.addLast(new int[] {startY, startX, 0});
 		
-		// 소용돌이인 경우 지나갈 수 있는 시간까지 기다리기
-		if (map[y][x] == 2) {
-			while(time % 3 != 0) time++;
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			if (!visited[y + dy[i]][x + dx[i]]) {
-				visited[y + dy[i]][x + dx[i]] = true;
-				DFS(y + dy[i], x + dx[i], time + 1);
-				visited[y + dy[i]][x + dx[i]] = false;
+		while(!deque.isEmpty()) {
+			int[] current = deque.pollFirst();
+			int time = current[2];
+			
+			if (current[0] == endY && current[1] == endX) {
+				return time;
 			}
+			
+			// 상하좌우 이동
+			for (int i = 0; i < 4; i++) {
+				int nextY = current[0] + dy[i];
+				int nextX = current[1] + dx[i];
+				
+				if (!visited[nextY][nextX] && map[nextY][nextX] != -1 && map[nextY][nextX] != 1) {
+
+					// 다음 타일이 소용돌이인 경우
+					if (map[nextY][nextX] == 2) {
+						if (time % 3 == 2) {
+							visited[nextY][nextX] = true;
+							deque.addLast(new int[] {nextY, nextX, time + 1});
+						} else {
+							deque.addLast(new int[] {current[0], current[1], time + 1});
+						}
+					} else {
+						visited[nextY][nextX] = true;
+	                    deque.addLast(new int[] {nextY, nextX, time + 1});
+					}
+				}
+			}
+			
 		}
+		
+		// 목적지에 도착한 시간 반환
+		return -1;
 	}
 }
