4d780cac8f1f719e9184dd0f892505ad80e555de
김정윤 <canach3@gmail.com>
2026-03-04 15:45:32 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 1,062 ms, Memory: 82,420 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -2,136 +2,129 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-        for (int testCase = 1; testCase <= T; testCase++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-
-            Deque<Node>[][] map = new Deque[N][N];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = new ArrayDeque<>();
-                }
-            }
-
-            for (int i = 0; i < K; i++) {
-                st = new StringTokenizer(br.readLine());
-                int y = Integer.parseInt(st.nextToken());
-                int x = Integer.parseInt(st.nextToken());
-                int q = Integer.parseInt(st.nextToken());
-                int d = Integer.parseInt(st.nextToken());
-
-                // 상:1 하:2 좌:3 우:4
-                map[y][x].addFirst(new Node(q, d));
-            }
-
-            for (int time = 0; time < M; time++) {
-                // 상 이동 : 상에서 하로 루프
-                for (int i = 1; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        if (!map[i][j].isEmpty() && map[i][j].peekLast().direction == 1) {
-                            map[i - 1][j].addFirst(map[i][j].pollLast());
-                        }
-                    }
-                }
-
-                // 하 이동 : 하에서 상으로 루프
-                for (int i = N - 2; i >= 0; i--) {
-                    for (int j = 0; j < N; j++) {
-                        if (!map[i][j].isEmpty() && map[i][j].peekLast().direction == 2) {
-                            map[i + 1][j].addFirst(map[i][j].pollLast());
-                        }
-                    }
-                }
-
-                // 좌 이동 : 좌에서 우로 루프
-                for (int i = 0; i < N; i++) {
-                    for (int j = 1; j < N; j++) {
-                        if (!map[i][j].isEmpty() && map[i][j].peekLast().direction == 3) {
-                            map[i][j - 1].addFirst(map[i][j].pollLast());
-                        }
-                    }
-                }
-
-                // 우 이동 : 우에서 좌로 루프
-                for (int i = 0; i < N; i++) {
-                    for (int j = N - 2; j >= 0; j--) {
-                        if (!map[i][j].isEmpty() && map[i][j].peekLast().direction == 4) {
-                            map[i][j + 1].addFirst(map[i][j].pollLast());
-                        }
-                    }
-                }
-
-                // 전체 탐색 : 총 미생물 수, 방향 설정 (cesco 체크)
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        Deque<Node> queue = map[i][j];
-
-                        // 가장자리인 경우
-                        if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
-                            // 방향 바꾸기
-                            if (!queue.isEmpty()) {
-                                Node node = queue.peekLast();
-
-                                if (node.direction == 1 || node.direction == 2) {
-                                    node.direction = 3 - node.direction;
-                                }
-
-                                if (node.direction == 3 || node.direction == 4) {
-                                    node.direction = 7 - node.direction;
-                                }
-
-                                node.quantity /= 2;
-                            }
-                        } else {
-                            // 겹치는 군집 합치기
-                            int max = 0;
-                            int max_d = 0;
-                            int sum = 0;
-
-                            while (!queue.isEmpty()) {
-                                Node node = queue.pollLast();
-                                sum += node.quantity;
-
-                                if (node.quantity > max) {
-                                    max = node.quantity;
-                                    max_d = node.direction;
-                                }
-                            }
-
-                            queue.addFirst(new Node(sum, max_d));
-                        }
-                    }
-                }
-            }
-
-            int total = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    Deque<Node> queue = map[i][j];
-                    if (!queue.isEmpty()) {
-                        total += queue.peekLast().quantity;
-                    }
-                }
-            }
-
-            System.out.println("#" + testCase + " " + total);
-        }
-    }
-
-    static class Node {
-        int quantity;
-        int direction;
-
-        Node(int quantity, int direction) {
-            this.quantity = quantity;
-            this.direction = direction;
-        }
-    }
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] dy = {0, -1, 1, 0, 0};
+		int[] dx = {0, 0, 0, -1, 1};
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			
+			Deque<Hatchery>[][] map = new Deque[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					map[i][j] = new ArrayDeque<>();
+				}
+			}
+			
+			Deque<Hatchery> deque = new ArrayDeque<>();
+			
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				int y = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken());
+				int amount = Integer.parseInt(st.nextToken());
+				int direction = Integer.parseInt(st.nextToken());
+				
+				deque.add(new Hatchery(y, x, amount, direction));
+			}
+			
+			int time = 1;
+			while(time <= M) {
+				
+				// 현존하는 군집 처리
+				while (!deque.isEmpty()) {
+					Hatchery h = deque.pollFirst();
+					
+					// 해당 방향으로 이동
+					h.y = h.y + dy[h.direction];
+					h.x = h.x + dx[h.direction];
+					
+					// 세스코 체크 (1/2 만큼 감소 후, 방향 전환)
+					if (h.y == 0 || h.y == N - 1 || h.x == 0 || h.x == N - 1) {
+						h.amount /= 2;
+						
+						if (h.direction == 1) h.direction = 2;
+						else if (h.direction == 2) h.direction = 1;
+						else if (h.direction == 3) h.direction = 4;
+						else if (h.direction == 4) h.direction = 3;
+					}
+					
+					// 현재 위치한 타일의 큐에 집어넣기
+					map[h.y][h.x].addLast(h);
+				}
+				
+				// 각 타일별 겹치는 군집 처리
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						
+						// 해당 타일에 군집이 존재하는 경우
+						if (!map[i][j].isEmpty()) {
+							
+							// 군집이 하나만 존재하는 경우 -> 그냥 큐에 넣기
+							if (map[i][j].size() == 1) {
+								deque.addLast(map[i][j].pollFirst());
+								continue;
+							}
+							
+							// 군집이 여러개 존재하는 경우 -> 합치고 큐에 넣기
+							int max = 0;
+							int direction = 0;
+							int sum = 0;
+							while(!map[i][j].isEmpty()) {
+								Hatchery h = map[i][j].pollFirst();
+								
+								sum += h.amount;
+								if (h.amount > max) {
+									max = h.amount;
+									direction = h.direction;
+								}
+							}
+							
+							deque.addLast(new Hatchery(i, j, sum, direction));
+						}
+					}
+				}
+				
+				time++;
+			}
+			
+			int sum = 0;
+			while (!deque.isEmpty()) {
+				sum += deque.pollFirst().amount;
+			}
+			
+			sb.append(sum).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+	
+	static class Hatchery implements Comparable<Hatchery> {
+		int y;
+		int x;
+		int amount;
+		int direction;
+		
+		Hatchery(int y, int x, int amount, int direction) {
+			this.y = y;
+			this.x = x;
+			this.amount = amount;
+			this.direction = direction;
+		}
+		
+		@Override
+		public int compareTo(Hatchery o) {
+			return Integer.compare(this.amount, o.amount);
+		}
+	}
 }
