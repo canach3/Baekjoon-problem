235eafcb1e605f7f81ec00945ebe818a6c19e085
김정윤 <canach3@gmail.com>
2026-03-05 09:07:17 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 135 ms, Memory: 39,000 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] dx = {0, 0, -1, 1};
+		int[] dy = {-1, 1, 0, 0};
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			int N = Integer.parseInt(br.readLine().trim());
+			int[][] map = new int[N + 2][N + 2];
+			int[][] cost = new int[N + 2][N + 2];
+			boolean visited[][] = new boolean[N + 2][N + 2];
+			
+			for (int i = 0; i < N + 2; i++) {
+				for (int j = 0; j < N + 2; j++) {
+					map[i][j] = -1;
+				}
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				String input = br.readLine().trim();
+				for (int j = 0; j < N; j++) {
+					map[i][j + 1] = input.charAt(j) - '0'; 
+				}
+			}
+			
+//			for (int i = 0; i < N + 2; i++) {
+//				System.out.println(Arrays.toString(map[i]));
+//			}
+			
+			Deque<int[]> deque = new ArrayDeque<>();
+			deque.addLast(new int[] {1, 1});
+			visited[1][1] = true;
+			
+			// 
+			while (!deque.isEmpty()) {
+				int[] current = deque.pollFirst();
+				int y = current[0];
+				int x = current[1];
+				
+				for (int i = 0; i < 4; i++) {
+					int nextY = y + dy[i];
+					int nextX = x + dx[i];
+					
+					// 경계인 경우 제외
+					if (map[nextY][nextX] == -1) continue;
+					
+					// 이미 방문한 경우 : 더 적은 비용으로 조착할 수 있으면 갱신 후 넣기
+					if (visited[nextY][nextX]) {
+						if (cost[nextY][nextX] > cost[y][x] + map[nextY][nextX]) {
+							cost[nextY][nextX] = cost[y][x] + map[nextY][nextX];
+							deque.addLast(new int[] {nextY, nextX});
+						}
+						
+					} else {	// 방문하지 않은 경우는 비용 갱신 후 넣기
+						visited[nextY][nextX] = true;
+						cost[nextY][nextX] = cost[y][x] + map[nextY][nextX];
+						deque.addLast(new int[] {nextY, nextX});
+					}
+				}
+			}
+			
+			sb.append(cost[N][N]).append("\n");
+		}
+		
+		System.out.print(sb);
+	}	
+}
