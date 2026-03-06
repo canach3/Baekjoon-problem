50ef55a59c0de9fc2fdd1945f31d1bca35627b67
김정윤 <canach3@gmail.com>
2026-03-06 23:14:52 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 180 ms, Memory: 29,124 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class Solution {
 	static int N;
 	static int[][] map;
+	static int maxCore;
+	static int minLine;
 	static List<int[]> cores;
 	
-	static int coreMax;
-	static int lineMin;
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
 	
 	public static void main(String[] args) throws IOException{
 		StringBuilder sb = new StringBuilder();
@@ -18,119 +20,146 @@ public class Solution {
 			sb.append("#").append(testCase).append(" ");
 			
 			N = Integer.parseInt(br.readLine().trim());
-			map = new int[N][N];
 			cores = new ArrayList<>();
 			
+			map = new int[N][N];
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					if (map[i][j] == 1) cores.add(new int[] {i, j});
+					
+					if (map[i][j] == 1) {
+						cores.add(new int[] {i, j});
+					}
 				}
 			}
-
-			coreMax = 0;
-			lineMin = Integer.MAX_VALUE;
+			
+			maxCore = 0;
+			minLine = Integer.MAX_VALUE;
 			DFS(0, 0, 0);
-		
-			sb.append(lineMin).append("\n");
+			
+			sb.append(minLine).append("\n");
 		}
 		
 		System.out.print(sb);
 	}
 	
-	static void DFS(int core, int coreCnt, int lineCnt) {
-		// 종료 조건 : 모든 core 다 확인한 경우
-		if (core >= cores.size()) {
-			if (coreCnt > coreMax) {
-				coreMax = coreCnt;
-				lineMin = lineCnt;
-			} else if (coreMax == coreCnt) {
-				lineMin = Math.min(lineMin, lineCnt);
+	static void DFS(int idx, int coreCnt, int lineCnt) {
+		// 종료조건 : 모든 core 검사 완료
+		if (idx == cores.size()) {
+			if (coreCnt > maxCore) {
+				maxCore = coreCnt;
+				minLine = lineCnt;
+			} else if (coreCnt == maxCore) {
+				minLine = Math.min(minLine, lineCnt);
 			}
 			
 			return;
 		}
 		
-		int coreY = cores.get(core)[0];
-		int coreX = cores.get(core)[1];
+		int y = cores.get(idx)[0];
+		int x = cores.get(idx)[1];
 		
-		// 가장자리인 경우
-		if (coreX == 0 || coreX == N - 1 || coreY == 0 || coreY == N - 1) {
-			DFS(core + 1, coreCnt + 1, lineCnt);
+		// core가 벽에 붙어있는 경유
+		if (y == 0 || y == N - 1 || x == 0 || x == N - 1) {
+			DFS(idx + 1, coreCnt + 1, lineCnt);
+			return;
 		}
 		
-		// 가장자리가 아닌 경우
-		else {
-			// 전원X
-			DFS(core + 1, coreCnt, lineCnt);
-			
-			// 상
-			boolean install = true;
-			int cnt = 0;
-			for (int i = coreY - 1; i >= 0; i--) {
-				if (map[i][coreX] == 1) {
-					install = false;
-					break;
-				}
-				cnt++;
+		// 전원을 연결하지 않은 경우
+		DFS(idx + 1, coreCnt, lineCnt);
+		
+		// 전원을 연결한 경우
+		
+		// 상
+		boolean isPossible = true;
+		int line = 0;
+		for (int i = 1; y - i >= 0; i++) {
+			if (map[y - i][x] == 1) {
+				isPossible = false;
+				break;
+			}
+		}
+		
+		if (isPossible) {
+			for (int i = 1; y - i >= 0; i++) {
+				map[y - i][x] = 1;
+				line++;
 			}
 			
-			if (install) {
-				for (int i = coreY - 1; i >= 0; i--) map[i][coreX] = 1;
-				DFS(core + 1, coreCnt + 1, lineCnt + cnt);
-				for (int i = coreY - 1; i >= 0; i--) map[i][coreX] = 0;
+			DFS(idx + 1, coreCnt + 1, lineCnt + line);
+			
+			for (int i = 1; y - i >= 0; i++) {
+				map[y - i][x] = 0;
+			}
+		}
+		
+		// 하
+		isPossible = true;
+		line = 0;
+		for (int i = 1; y + i < N; i++) {
+			if (map[y + i][x] == 1) {
+				isPossible = false;
+				break;
+			}
+		}
+		
+		if (isPossible) {
+			for (int i = 1; y + i < N; i++) {
+				map[y + i][x] = 1;
+				line++;
 			}
 			
-			// 하
-			install = true;
-			cnt = 0;
-			for (int i = coreY + 1; i < N; i++) {
-				if (map[i][coreX] == 1) {
-					install = false;
-					break;
-				}
-				cnt++;
+			DFS(idx + 1, coreCnt + 1, lineCnt + line);
+			
+			for (int i = 1; y + i < N; i++) {
+				map[y + i][x] = 0;
+			}
+		}
+		
+		// 좌
+		isPossible = true;
+		line = 0;
+		for (int i = 1; x - i >= 0; i++) {
+			if (map[y][x - i] == 1) {
+				isPossible = false;
+				break;
+			}
+		}
+		
+		if (isPossible) {
+			for (int i = 1; x - i >= 0; i++) {
+				map[y][x - i] = 1;
+				line++;
 			}
 			
-			if (install) {
-				for (int i = coreY + 1; i < N; i++) map[i][coreX] = 1;
-				DFS(core + 1, coreCnt + 1, lineCnt + cnt);
-				for (int i = coreY + 1; i < N; i++) map[i][coreX] = 0;
+			DFS(idx + 1, coreCnt + 1, lineCnt + line);
+			
+			for (int i = 1; x - i >= 0; i++) {
+				map[y][x - i] = 0;
+			}
+		}
+		
+		// 우
+		isPossible = true;
+		line = 0;
+		for (int i = 1; x + i < N; i++) {
+			if (map[y][x + i] == 1) {
+				isPossible = false;
+				break;
+			}
+		}
+		
+		if (isPossible) {
+			for (int i = 1; x + i < N; i++) {
+				map[y][x + i] = 1;
+				line++;
 			}
 			
-			// 좌
-			install = true;
-			cnt = 0;
-			for (int i = coreX - 1; i >= 0; i--) {
-				if (map[coreY][i] == 1) {
-					install = false;
-					break;
-				}
-				cnt++;
-			}
+			DFS(idx + 1, coreCnt + 1, lineCnt + line);
 			
-			if (install) {
-				for (int i = coreX - 1; i >= 0; i--) map[coreY][i] = 1;
-				DFS(core + 1, coreCnt + 1, lineCnt + cnt);
-				for (int i = coreX - 1; i >= 0; i--) map[coreY][i] = 0;
-			}
-			
-			// 우
-			install = true;
-			cnt = 0;
-			for (int i = coreX + 1; i < N; i++) {
-				if (map[coreY][i] == 1) {
-					install = false;
-					break;
-				}
-				cnt++;
-			}
-			
-			if (install) {
-				for (int i = coreX + 1; i < N; i++) map[coreY][i] = 1;
-				DFS(core + 1, coreCnt + 1, lineCnt + cnt);
-				for (int i = coreX + 1; i < N; i++) map[coreY][i] = 0;
+			for (int i = 1; x + i < N; i++) {
+				map[y][x + i] = 0;
 			}
 		}
 	}
