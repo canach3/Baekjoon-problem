28c3f0e3623913eca2f7889743911461c3a12bbd
김정윤 <canach3@gmail.com>
2026-03-11 15:18:26 +0900

    [D4] Title: 햄스터, Time: 1,655 ms, Memory: 116,084 KB -BaekjoonHub

--- SWEA/D4/8275. 햄스터/햄스터.java
+++ SWEA/D4/8275. 햄스터/햄스터.java
@@ -60,9 +60,11 @@ public class Solution {
 				sumArr[i] = sumArr[i - 1] + arr[i];
 			}
 			
-			boolean isOk = true;
+			// max 값보다 작은 경우 제외
+			if (sumArr[N] < max) return;
 			
 			// 현재 결과가 조건들에 부합하는지 확인 (정답이 되는지)
+			boolean isOk = true;
 			for (int i = 0; i < M; i++) {
 				int left = commands.get(i)[0];
 				int right = commands.get(i)[1];
@@ -78,30 +80,27 @@ public class Solution {
 			if (!isOk) return;
 			
 			// 정답으로 가능한 경우
-			// 우선순위 1 : 햄스터 수가 가장 많은 것
-			if (sumArr[N] > max) {
-				if (sumArr[N] > max) {
-					max = sumArr[N];
-					result = Arrays.copyOf(arr, N + 1);
+			isPossible = true;
+			
+			// 햄스터 수가 동일하면 사전 순으로 앞선 것
+			if (sumArr[N] == max) {
+				boolean win = true;
+				
+				for (int i = 1; i <= N; i++) {
+					if (arr[i] > result[i]) {
+						win = false;
+						break;
+					}
 				}
 				
-				// 우선순위 2 : 사전 순으로 앞선 것
-				else if (sumArr[N] == max) {
-					boolean win = false;
-					
-					for (int i = 1; i <= N; i++) {
-						if (arr[i] < result[i]) {
-							win = true;
-							break;
-						}
-					}
-					
-					if (win) result = Arrays.copyOf(arr, N + 1);
-				}
-			}
-
-			isPossible = true;
+				if (win) result = Arrays.copyOf(arr, N + 1);
+				return;
+			} 
+			
+			max = sumArr[N];
+			result = Arrays.copyOf(arr, N + 1);
 			return;
+			
 		}
 		
 		for (int i = 0; i <= X; i++) {
