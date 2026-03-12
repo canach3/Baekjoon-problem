6fb353503b88bb2196b6a571539605fb8ed5e889
김정윤 <canach3@gmail.com>
2026-03-13 08:36:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 142172 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,10 +1,9 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static List<List<Integer>> adjList;
     static boolean[] visited;
-    static List<Integer>[] adjList;
-    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,40 +12,37 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N + 1];
-
-        adjList = new List[N + 1];
+        adjList = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
-            adjList[i] = new ArrayList<>();
+            adjList.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int vertex1 = Integer.parseInt(st.nextToken());
-            int vertex2 = Integer.parseInt(st.nextToken());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
 
-            adjList[vertex1].add(vertex2);
-            adjList[vertex2].add(vertex1);
+            adjList.get(v1).add(v2);
+            adjList.get(v2).add(v1);
         }
 
+        visited = new boolean[N + 1];
+        int cnt = 0;
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
-                cnt++;
                 DFS(i);
+                cnt++;
             }
         }
-
 
         System.out.println(cnt);
     }
 
-    static void DFS(int start) {
-        visited[start] = true;
+    static void DFS(int current) {
+        visited[current] = true;
 
-        for (int next : adjList[start]) {
-            if (!visited[next]) {
-                DFS(next);
-            }
+        for (Integer next : adjList.get(current)) {
+            if (!visited[next]) DFS(next);
         }
     }
 }
