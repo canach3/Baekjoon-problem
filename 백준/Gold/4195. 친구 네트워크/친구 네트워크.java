83e54f98ee5f4b4eb0b79b3149dafe970cae99a7
김정윤 <canach3@gmail.com>
2026-03-17 12:03:49 +0900

    [Gold II] Title: 친구 네트워크, Time: 652 ms, Memory: 65940 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static Map<String, Integer> hashMap;
+	static int[] parent;
+	static int[] depth;
+	static int idx;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int F = Integer.parseInt(br.readLine().trim());
+			
+			// 변수 초기화
+			hashMap = new HashMap<>();
+			parent = new int[F * 2 + 1];
+			depth = new int[F * 2 + 1];
+			for (int i = 0; i < F * 2 + 1; i++) {
+				parent[i] = i;
+				depth[i] = 1;
+			}
+			
+			idx = -1;
+			
+			for (int i = 0; i < F; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int v1 = vId(st.nextToken());
+				int v2 = vId(st.nextToken());
+				
+				union(v1, v2);
+				
+				sb.append(depth[parent[v1]]).append("\n");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+	
+	static int vId(String input) {
+		if (!hashMap.containsKey(input)) {
+			hashMap.put(input, ++idx);
+			
+			return hashMap.get(input);
+		}
+		
+		return hashMap.get(input);
+	}
+	
+	static void union(int v1, int v2) {
+		int v1Root = find(v1);
+		int v2Root = find(v2);
+		
+		if (v1Root == v2Root) return;
+		
+		// depth 추가
+		parent[v2Root] = v1Root;
+		depth[v1Root] += depth[v2Root];
+		
+		
+	}
+	
+	static int find(int v) {
+		if (parent[v] == v) return v;
+		
+		return parent[v] = find(parent[v]);
+	}
+}
