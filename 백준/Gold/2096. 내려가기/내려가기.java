44b8bd0d1d55f6cd82c47469448582845c8c6b53
김정윤 <canach3@gmail.com>
2026-03-18 17:10:30 +0900

    [Gold V] Title: 내려가기, Time: 360 ms, Memory: 45768 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -3,19 +3,23 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[][] dpMax;
-    static int[][] dpMin;
+    static int[] dpMax;
+    static int[] dpMin;
     static int[][] scores;
+
+    static int idx_0;
+    static int idx_1;
+    static int idx_2;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine().trim());
 
-        dpMax = new int[N + 1][3];
-        dpMin = new int[N + 1][3];
-        scores = new int[N + 1][3];
+        dpMax = new int[3];
+        dpMin = new int[3];
+        scores = new int[N][3];
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             scores[i][0] = Integer.parseInt(st.nextToken());
             scores[i][1] = Integer.parseInt(st.nextToken());
@@ -23,25 +27,31 @@ public class Main {
         }
 
         // 첫째줄 값 초기화
-        dpMax[1][0] = scores[1][0];
-        dpMax[1][1] = scores[1][1];
-        dpMax[1][2] = scores[1][2];
-        dpMin[1][0] = scores[1][0];
-        dpMin[1][1] = scores[1][1];
-        dpMin[1][2] = scores[1][2];
+        dpMax[0] = scores[0][0];
+        dpMax[1] = scores[0][1];
+        dpMax[2] = scores[0][2];
+        dpMin[0] = scores[0][0];
+        dpMin[1] = scores[0][1];
+        dpMin[2] = scores[0][2];
 
-        for (int i = 2; i <= N; i++) {
-            dpMax[i][0] = Math.max(dpMax[i - 1][0], dpMax[i - 1][1]) + scores[i][0];
-            dpMax[i][1] = Math.max(dpMax[i - 1][0], Math.max(dpMax[i - 1][1], dpMax[i - 1][2])) + scores[i][1];
-            dpMax[i][2] = Math.max(dpMax[i - 1][1], dpMax[i - 1][2]) + scores[i][2];
+        for (int i = 1; i < N; i++) {
+            idx_0 = dpMax[0];
+            idx_1 = dpMax[1];
+            idx_2 = dpMax[2];
+            dpMax[0] = Math.max(idx_0, idx_1) + scores[i][0];
+            dpMax[1] = Math.max(idx_0, Math.max(idx_1, idx_2)) + scores[i][1];
+            dpMax[2] = Math.max(idx_1, idx_2) + scores[i][2];
 
-            dpMin[i][0] = Math.min(dpMin[i - 1][0], dpMin[i - 1][1]) + scores[i][0];
-            dpMin[i][1] = Math.min(dpMin[i - 1][0], Math.min(dpMin[i - 1][1], dpMin[i - 1][2])) + scores[i][1];
-            dpMin[i][2] = Math.min(dpMin[i - 1][1], dpMin[i - 1][2]) + scores[i][2];
+            idx_0 = dpMin[0];
+            idx_1 = dpMin[1];
+            idx_2 = dpMin[2];
+            dpMin[0] = Math.min(idx_0, idx_1) + scores[i][0];
+            dpMin[1] = Math.min(idx_0, Math.min(idx_1, idx_2)) + scores[i][1];
+            dpMin[2] = Math.min(idx_1, idx_2) + scores[i][2];
         }
 
-        int max = Math.max(dpMax[N][0], Math.max(dpMax[N][1], dpMax[N][2]));
-        int min = Math.min(dpMin[N][0], Math.min(dpMin[N][1], dpMin[N][2]));
+        int max = Math.max(dpMax[0], Math.max(dpMax[1], dpMax[2]));
+        int min = Math.min(dpMin[0], Math.min(dpMin[1], dpMin[2]));
 
         System.out.println(max + " " + min);
     }
