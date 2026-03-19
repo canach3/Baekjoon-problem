f571cc7f9107f5df1943c107ea77aba0e96e6dec
김정윤 <canach3@gmail.com>
2026-03-19 09:57:06 +0900

    [Silver II] Title: 생태학, Time: 564 ms, Memory: 74808 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -4,28 +4,41 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		Map<String, Integer> treeMap = new TreeMap<>();
+		int[] treeCnt = new int[10000];
+		List<String> treeList = new ArrayList<>();
+		Map<String, Integer> hashMap = new HashMap<>();
+		
+		int size = 0;
 		int totalCnt = 0;
 		
-		String treeName;
-		while((treeName = br.readLine()) != null) {
-			treeMap.put(treeName, treeMap.getOrDefault(treeName, 0) + 1);
-			totalCnt++;
+		String tree;
+		while((tree = br.readLine()) != null) {
+			Integer idx = hashMap.get(tree);
+			
+			if (idx == null) {
+				hashMap.put(tree, size++);
+				treeList.add(tree);
+				treeCnt[size - 1]++;
+				totalCnt++;
+			} else {
+				treeCnt[idx]++;
+				totalCnt++;
+			}
 		}
 		
-		StringBuilder sb = new StringBuilder();
-		for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
-		    String tree = entry.getKey();
-		    int count = entry.getValue();
-		    
-		    sb.append(tree).append(" ");
-		    
-		    double percentage = (double) count / totalCnt * 100.0;
-		    sb.append(String.format("%.4f", percentage)).append("\n");
-		}
+		Collections.sort(treeList);
+		
+		for (int i = 0; i < treeList.size(); i++) {
+			sb.append(treeList.get(i)).append(" ");
 			
+			int idx = hashMap.get(treeList.get(i));
+			
+			sb.append(String.format("%.4f", (double) treeCnt[idx] / totalCnt * 100.0)).append("\n");
+		}
+		
 		System.out.print(sb);
 	}
 }
