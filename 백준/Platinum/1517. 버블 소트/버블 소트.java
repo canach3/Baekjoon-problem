6aee352cc86a8f47656b8e4f5aa939153f20271b
김정윤 <canach3@gmail.com>
2026-03-19 15:23:33 +0900

    [Platinum V] Title: 버블 소트, Time: 520 ms, Memory: 74084 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -1,63 +1,68 @@
-import java.io.*;
-import java.nio.Buffer;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static long result;
+    static int[] numArr;
+    static int[] tmpArr;
+    static long cnt;
+
     public static void main(String[] args) throws IOException {
-        result = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] numArr = new int[N];
-        int[] temp = new int[N];
+        numArr = new int[N+1];
+        tmpArr = new int[N+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             numArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        mergeSort(numArr, temp);
-        System.out.println(result);
+        mergerSort(1, N);
+
+        System.out.println(cnt);
     }
 
-    public static void mergeSort(int[] arr, int[] temp) {
-        mergeSort(arr, temp, 0, arr.length - 1);
-    }
-
-    private static void mergeSort(int[] arr, int[] temp, int start, int end) {
-        if (start >= end) {
+    static void mergerSort(int start, int end) {
+        if (end - start < 1) {
             return;
         }
 
-        int mid = (start + end) / 2;
-        mergeSort(arr, temp, start, mid);
-        mergeSort(arr, temp, mid + 1, end);
-        merge(arr, temp, start, mid, end);
-    }
+        int middle = start + (end - start) / 2;
+        mergerSort(start, middle);
+        mergerSort(middle + 1, end);
 
-    private static void merge(int[] arr, int[] temp, int start, int mid, int end) {
         for (int i = start; i <= end; i++) {
-            temp[i] = arr[i];
+            tmpArr[i] = numArr[i];
         }
-        int part1 = start;
-        int part2 = mid + 1;
-        int index = start;
 
-        while (part1 <= mid && part2 <= end) {
-            if (temp[part1] <= temp[part2]) {
-                arr[index] = temp[part1];
-                part1++;
+        int idx = start;
+        int index1 = start;
+        int index2 = middle + 1;
+
+        while ((index1 <= middle) && (index2 <= end)) {
+            if (tmpArr[index1] <= tmpArr[index2]) {
+                numArr[idx] = tmpArr[index1];
+                idx++;
+                index1++;
             } else {
-                result += mid + 1 - part1;
-                arr[index] = temp[part2];
-                part2++;
+                numArr[idx] = tmpArr[index2];
+                cnt += index2 - idx;
+                idx++;
+                index2++;
             }
-            index++;
         }
 
-        for (int i = 0; i <= mid - part1; i++) {
-            arr[index + i] = temp[part1 + i];
+        while (index1 <= middle) {
+            numArr[idx] = tmpArr[index1];
+            idx++;
+            index1++;
+        }
+
+        while (index2 <= end) {
+            numArr[idx] = tmpArr[index2];
+            idx++;
+            index2++;
         }
     }
 }
