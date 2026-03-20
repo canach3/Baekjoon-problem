ed29b0abb508c9aa8789132e1f6171c5bb188f62
김정윤 <canach3@gmail.com>
2026-03-20 10:06:10 +0900

    [Gold IV] Title: 전화번호 목록, Time: 628 ms, Memory: 32744 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,63 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
-	static Map<String, Integer> map;
-	static List<String>[] numList;
-	
-	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine().trim());
-		for (int testCase = 1; testCase <= T; testCase++) {
-			int N = Integer.parseInt(br.readLine().trim());
-
-			map = new HashMap<>();
-			numList = new List[11];
-			for (int i = 0; i <= 10; i++) {
-				numList[i] = new ArrayList<>();
-			}
-			
-			boolean isSame = false;
-			
-			for (int i = 0; i < N; i++) {
-				String num = br.readLine().trim();
-				
-				numList[num.length()].add(num);
-			}
-			
-			for (int i = 1; i <= 10; i++) {
-				List<String> list = numList[i];
-				
-				for (int j = 0; j < list.size(); j++) {
-					String num = list.get(j);
-					
-					for (int k = 1; k <= num.length(); k++) {
-						String tmp = num.substring(0, k);
-						
-						// 발견하는 즉시 빠져나오기
-						if(map.containsKey(tmp)) {
-							sb.append("NO").append("\n");
-							isSame = true;
-							break;
-						}
-					}
-					
-					if (isSame) break;
-					
-					// 마지막엔 자기 자신값 map 에 저장
-					map.put(num,  1);
-				}
-				
-				if (isSame) break;
-			}
-			
-			if (isSame) continue;
-			
-			sb.append("YES").append("\n");
-		}
-	
-		System.out.print(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int t = Integer.parseInt(br.readLine()); 
+        
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine()); 
+            String[] phoneBook = new String[n];
+            
+            for (int i = 0; i < n; i++) {
+                phoneBook[i] = br.readLine();
+            }
+            
+            Arrays.sort(phoneBook);
+            
+            boolean isConsistent = true;
+            
+            for (int i = 0; i < n - 1; i++) {
+                if (phoneBook[i + 1].startsWith(phoneBook[i])) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            
+            if (isConsistent) {
+                sb.append("YES\n");
+            } else {
+                sb.append("NO\n");
+            }
+        }
+        
+        System.out.print(sb.toString());
+    }
 }
