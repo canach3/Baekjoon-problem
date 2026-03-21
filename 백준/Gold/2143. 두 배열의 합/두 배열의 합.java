4cba1206fe873f3c7a6721a4439a58f675c8147a
김정윤 <canach3@gmail.com>
2026-03-21 11:36:29 +0900

    [Gold III] Title: 두 배열의 합, Time: 496 ms, Memory: 94404 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -2,81 +2,55 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int T = Integer.parseInt(br.readLine().trim());
-        
+
+        // a, b의 누적합 구하기 O(n) -> 2,000
         int N = Integer.parseInt(br.readLine().trim());
-        int[] nSumArr = new int[N + 1];
+        int[] A = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            nSumArr[i] = nSumArr[i - 1] + Integer.parseInt(st.nextToken());
+            A[i] = A[i - 1] + Integer.parseInt(st.nextToken());
         }
-        
+
         int M = Integer.parseInt(br.readLine().trim());
-        int[] mSumArr = new int[M + 1];
+        int[] B = new int[M + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= M; i++) {
-            mSumArr[i] = mSumArr[i - 1] + Integer.parseInt(st.nextToken());
+            B[i] = B[i - 1] + Integer.parseInt(st.nextToken());
         }
-        
-        // 1. 부배열 크기 공식으로 한 번에 계산
-        int sizeA = N * (N + 1) / 2;
-        int[] tmpA = new int[sizeA];
-        int idx = 0;
-        for (int i = 0; i <= N - 1; i++) {
+
+        // a, b 각각 모든 부배열의 합 구해서 HashMap 에 저장 -> 1,001,000
+        Map<Integer, Integer> aMap = new HashMap<>();
+        for (int i = 0; i < N; i++) {
             for (int j = i + 1; j <= N; j++) {
-                tmpA[idx++] = nSumArr[j] - nSumArr[i];
+                int num = A[j] - A[i];
+                if (aMap.get(num) == null) aMap.put(num, 1);
+                else aMap.put(num, aMap.get(num) + 1);
             }
         }
-        
-        int sizeB = M * (M + 1) / 2;
-        int[] tmpB = new int[sizeB];
-        idx = 0;
-        for (int i = 0; i <= M - 1; i++) {
+
+        Map<Integer, Integer> bMap = new HashMap<>();
+        for (int i = 0; i < M; i++) {
             for (int j = i + 1; j <= M; j++) {
-                tmpB[idx++] = mSumArr[j] - mSumArr[i];
+                int num = B[j] - B[i];
+                if (bMap.get(num) == null) bMap.put(num, 1);
+                else bMap.put(num, bMap.get(num) + 1);
             }
         }
-        
-        // 부배열 정렬
-        Arrays.sort(tmpA);
-        Arrays.sort(tmpB);
-        
-        // 2. List 압축 및 이진 탐색 대신 투 포인터 적용
-        long result = 0;
-        int left = 0;
-        int right = tmpB.length - 1;
-        
-        while (left < tmpA.length && right >= 0) {
-            long sum = (long) tmpA[left] + tmpB[right]; // 오버플로우 방지
-            
-            if (sum == T) {
-                int currentA = tmpA[left];
-                long countA = 0;
-                // A 배열에서 중복된 값 개수 한 번에 세기
-                while (left < tmpA.length && tmpA[left] == currentA) {
-                    countA++;
-                    left++;
-                }
-                
-                int currentB = tmpB[right];
-                long countB = 0;
-               
-                while (right >= 0 && tmpB[right] == currentB) {
-                    countB++;
-                    right--;
-                }
-                
-                result += countA * countB;
-                
-            } else if (sum < T) {
-                left++; 
-            } else {
-                right--; 
-            }
+
+        // T - a = b 가 존재하는 지 찾기 O(n) -> 500500
+        long cnt = 0;
+        for (Integer aKey : aMap.keySet()) {
+            int aValue = aMap.get(aKey);
+            int bKey = T - aKey;
+
+            Integer bValue = bMap.get(bKey);
+            if (bValue != null) cnt += (long) aValue * bValue;
         }
-        
-        System.out.println(result);
+
+        System.out.println(cnt);
     }
 }
