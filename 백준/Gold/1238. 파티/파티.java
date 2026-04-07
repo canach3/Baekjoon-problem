7a3eb8a2f5e34f3be23ae29a80b039470057ebe5
김정윤 <canach3@gmail.com>
2026-04-08 08:55:58 +0900

    [Gold III] Title: 파티, Time: 304 ms, Memory: 21484 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,9 +3,11 @@ import java.util.*;
 
 public class Main {
     static int N;
+    static int X;
     static int[] dist;
     static int[][] totalCost;
     static List<int[]>[] adjList;
+    static List<int[]>[] reverseAdjList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,26 +15,33 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
         adjList = new List[N + 1];
         for (int i = 1; i <= N; i++) {
             adjList[i] = new ArrayList<>();
         }
 
+        reverseAdjList = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            reverseAdjList[i] = new ArrayList<>();
+        }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            adjList[Integer.parseInt(st.nextToken())].add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            adjList[u].add(new int[]{v, cost});
+            reverseAdjList[v].add(new int[]{u, cost});
         }
 
         dist = new int[N + 1];
         totalCost = new int[N + 1][2];
 
-        for (int i = 1; i <= N; i++) {
-            if (i == X) continue;
-            dijkstra(i, X);
-        }
-        dijkstra(X, -1);
+        dijkstra();
+        rDijkstra();
 
         int max = 0;
         for (int i = 1; i <= N; i++) {
@@ -42,21 +51,19 @@ public class Main {
         System.out.println(max);
     }
 
-    static void dijkstra(int start, int end) {
+    static void dijkstra() {
         Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
+        dist[X] = 0;
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-        pq.add(new int[]{start, 0});
+        pq.add(new int[]{X, 0});
 
         while (!pq.isEmpty()) {
             int[] cTmp = pq.poll();
             int curr = cTmp[0];
             int cost = cTmp[1];
 
-            if (curr == end) break;
             if (cost > dist[curr]) continue;
-
             for (int[] nTmp : adjList[curr]) {
                 int next = nTmp[0];
                 int newCost = cost + nTmp[1];
@@ -68,10 +75,34 @@ public class Main {
             }
         }
 
-        if (end == -1) {
-            for (int i = 1; i <= N; i++) totalCost[i][1] = dist[i];
-        } else {
-            totalCost[start][0] = dist[end];
+        for (int i = 1; i <= N; i++) totalCost[i][0] = dist[i];
+    }
+
+    static void rDijkstra() {
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[X] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        pq.add(new int[]{X, 0});
+
+        while (!pq.isEmpty()) {
+            int[] cTmp = pq.poll();
+            int curr = cTmp[0];
+            int cost = cTmp[1];
+
+            if (cost > dist[curr]) continue;
+
+            for (int[] nTmp : reverseAdjList[curr]) {
+                int next = nTmp[0];
+                int newCost = cost + nTmp[1];
+
+                if (newCost < dist[next]) {
+                    dist[next] = newCost;
+                    pq.add(new int[]{next, newCost});
+                }
+            }
         }
+
+        for (int i = 1; i <= N; i++) totalCost[i][1] = dist[i];
     }
 }
