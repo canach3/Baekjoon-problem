37042a72fc7e8aa98ff64ff9f0a5360cb10d6a63
김정윤 <canach3@gmail.com>
2026-04-07 10:43:26 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,31 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static StringBuilder sb;
+    static int N;
+    static int M;
+    static boolean[] check;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        check = new boolean[N + 1];
 
-        List<Integer> list = new ArrayList<>();
-        dfs(1, list);
+        sequence(1, new int[M + 1]);
+
+        System.out.print(sb);
     }
 
-    static void dfs(int start, List<Integer> list) {
-        if (list.size() == M) {
-            StringBuilder sb = new StringBuilder();
-            for (int num : list) sb.append(num).append(' ');
-            System.out.println(sb);
+    static void sequence(int idx, int[] arr) {
+        if (idx > M) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+
             return;
         }
 
-        for (int i = start; i <= N; i++) {
-            list.add(i);
-            dfs(i + 1, list);
-            list.remove(list.size() - 1);
+        for (int i = arr[idx - 1] + 1; i <= N; i++) {
+            if (check[i]) continue;
+
+            check[i] = true;
+            arr[idx] = i;
+            sequence(idx + 1, arr);
+            check[i] = false;
         }
     }
 }
