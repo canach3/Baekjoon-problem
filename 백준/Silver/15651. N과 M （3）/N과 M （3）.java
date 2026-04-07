ac3952ce1ca1d96971ed6bc3075417edcbcd2302
김정윤 <canach3@gmail.com>
2026-04-07 10:46:55 +0900

    [Silver III] Title: N과 M (3), Time: 368 ms, Memory: 111948 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,31 +1,37 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-	static int N;
-	static int M;
-	static int[] arr;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		arr = new int[M];
-		
-		dfs(0);
-		System.out.print(sb);
-		
-	}
-	private static void dfs(int idx) {
-		if(idx == M) {
-			for(int i = 0; i < M; i++) {
-				sb.append(arr[i]).append(" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for(int i = 1; i <= N; i++) {
-			arr[idx] = i;
-			dfs(idx + 1);
-		}
-	}
+    static StringBuilder sb;
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+
+        sequence(1, new int[M + 1]);
+
+        System.out.print(sb);
+    }
+
+    static void sequence(int idx, int[] arr) {
+        if (idx > M) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[idx] = i;
+            sequence(idx + 1, arr);
+        }
+    }
 }
