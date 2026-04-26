47c18c309e54f870d4e1468e6bf57d4ed68766cb
김정윤 <canach3@gmail.com>
2026-04-27 08:37:45 +0900

    [D4] Title: 치즈 도둑, Time: 331 ms, Memory: 95,872 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class Solution {
     static int N;
     static int[][] map;
-    static int[][] cheeseArr;
-    static int[] parent;
+    static boolean[][] visited1;
+    static boolean[][] visited2;
 
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -19,87 +19,86 @@ public class Solution {
             sb.append("#").append(testCase).append(" ");
 
             N = Integer.parseInt(br.readLine().trim());
-            map = new int[N][N];
-            cheeseArr = new int[N * N][2];
+            map = new int[N + 2][N + 2];
 
-            parent = new int [N * N];
-            for (int i = 0; i < N * N; i++) {
-                parent[i] = i;
-            }
-
-            int days = 0;
-            for (int i = 0; i < N; i++) {
+            for (int i = 1; i <= N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    int taste = Integer.parseInt(st.nextToken());
-                    map[i][j] = taste;
-                    cheeseArr[N * i + j][0] = N * i + j;
-                    cheeseArr[N * i + j][1] = taste;
-
-                    days = Math.max(days, taste);
+                for (int j = 1; j <= N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            // taste 기준으로 내림차순 정렬
-            Arrays.sort(cheeseArr, (a, b) -> Integer.compare(b[1], a[1]));
+            visited1 = new boolean[N + 2][N + 2];
+            int max = 1;
 
-            int maxCnt = 1;
-            int cnt = 0;
-            int start = 0;
+            for (int day = 1; day <= 100; day++) {
+                int cnt = countGroup(day);
+                if (cnt == 0) break;
 
-            for (int day = days; day > 0; day--) {
-                for (int i = start; i < N * N; i++) {
-                    int idx = cheeseArr[i][0];
-                    int taste = cheeseArr[i][1];
-
-                    // 해당 day와 동일한 taste인지 체크
-                    if (taste != day) {
-                        start = i;
-                        break;
-                    }
-
-                    cnt++;
-
-                    int y = idx / N;
-                    int x = idx % N;
-                    for (int j = 0; j < 4; j++) {
-                        int nextY = y + dy[j];
-                        int nextX = x + dx[j];
-                        int nextIdx = N * nextY + nextX;
-
-                        if (nextY == -1 || nextY == N || nextX == -1 || nextX == N) continue;
-
-                        // 상하좌우에 더 높은 taste 가 있는 경우 : 루트가 다른 경우 cnt--;
-                        if (map[nextY][nextX] >= taste) {
-                            if (union(idx, nextIdx)) {
-                                cnt--;
-                            }
-                        }
-                    }
-                }
-
-                maxCnt = Math.max(maxCnt, cnt);
+                max = Math.max(max, cnt);
             }
 
-            sb.append(maxCnt).append("\n");
+            sb.append(max).append("\n");
         }
 
         System.out.print(sb);
     }
 
-    static boolean union(int v1, int v2) {
-        int v1Root = find(v1);
-        int v2Root = find(v2);
+    static int countGroup(int day) {
+        int cnt = 0;
+        visited2 = new boolean[N + 2][N + 2];
 
-        if (v1Root == v2Root) return false;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (map[i][j] == 0 || visited1[i][j] || visited2[i][j]) continue;
 
-        parent[v2Root] = v1Root;
-        return true;
+                // 치즈를 먹어야 하는 경우
+                if (map[i][j] == day) {
+                    BFS(i, j, day, true);
+                }
+
+                // 덩어리로 세어야 하는 경우
+                else {
+                    BFS(i, j, day, false);
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
     }
 
-    static int find(int v) {
-        if (parent[v] == v) return v;
+    static void BFS(int y, int x, int day, boolean isVisited1) {
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.addLast(new int[] {y, x});
 
-        return parent[v] = find(parent[v]);
+        if (isVisited1) visited1[y][x] = true;
+        else visited2[y][x] = true;
+
+        while (!deque.isEmpty()) {
+            int[] tmp = deque.pollFirst();
+            int currY = tmp[0];
+            int currX = tmp[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextY = currY + dy[i];
+                int nextX = currX + dx[i];
+
+                if (map[nextY][nextX] == 0) continue;
+                if ((isVisited1 && visited1[nextY][nextX]) || (!isVisited1 && visited2[nextY][nextX])) continue;
+
+                if (isVisited1) {
+                    if (map[nextY][nextX] == day) {
+                        visited1[nextY][nextX] = true;
+                        deque.addLast(new int[] {nextY, nextX});
+                    }
+                } else {
+                    if (!visited1[nextY][nextX] && map[nextY][nextX] != day) {
+                        visited2[nextY][nextX] = true;
+                        deque.addLast(new int[] {nextY, nextX});
+                    }
+                }
+            }
+        }
     }
 }
